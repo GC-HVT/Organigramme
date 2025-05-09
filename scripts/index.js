4bcea2a41cb02fafd15fe758a6c5f44c4f5a76af
GC-HVT <guillaume.collineau@herve-thermique.com>
2025-05-09 10:33:51 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,41 @@ import { setupEditor } from './modaleEditor.js';
 import { setupStorage } from './storage.js';
 
 let graph, paper;
+let currentGroup = 'Groupe A';
 
 document.addEventListener('DOMContentLoaded', () => {
-({ graph, paper } = initGraph('myDiagramDiv'));
+  ({ graph, paper } = initGraph('myDiagramDiv'));
+  window.graph = graph;
 
-document.getElementById('generateBtn').addEventListener('click', () => {
-generateStructure(graph);
+  const groupSelector = document.getElementById('groupSelector');
+  currentGroup = groupSelector.value;
+
+  loadGroupData(currentGroup);
+
+  document.getElementById('generateBtn').addEventListener('click', () => {
+    generateStructure(graph);
+    saveGroupData(currentGroup);
+  });
+
+  groupSelector.addEventListener('change', () => {
+    currentGroup = groupSelector.value;
+    loadGroupData(currentGroup);
+  });
+
+  setupEditor();
+  setupStorage(graph, () => saveGroupData(currentGroup));
 });
 
-setupEditor();
-setupStorage(graph);
-});
+function saveGroupData(group) {
+  const json = JSON.stringify(graph.toJSON());
+  localStorage.setItem(`organigramme_${group}`, json);
+}
+
+function loadGroupData(group) {
+  const data = localStorage.getItem(`organigramme_${group}`);
+  if (data) {
+    graph.fromJSON(JSON.parse(data));
+  } else {
+    graph.clear();
+  }
+}
